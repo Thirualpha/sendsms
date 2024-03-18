f9f863cd9bf5a7a535fe55eb0226dd6545147910
Thirualpha <113031702+Thirualpha@users.noreply.github.com>
2024-03-19 01:28:37 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sendsms/MainActivity.java
+++ app/src/main/java/com/example/sendsms/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.sendsms;
-
+    package com.example.sendingsms;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mbutton=(Button) findViewById(R.id.smsButton);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","9360247533",null));
+                intent.putExtra("sms_body","SMS using Intent");
+                startActivity(intent);
+            }
+        });
     }
 }
